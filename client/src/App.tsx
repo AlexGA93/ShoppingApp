fe9a45ef6802e5124f1b46d777f9f5a5afd782a7
Alejandro Gimeno <32788331+AlexGA93@users.noreply.github.com>
2022-02-04 17:12:35 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,24 +8,21 @@ import MainPage from './components/Pages/MainPage/MainPage';
 import Store from './components/Pages/Store/Store';
 import Favorites from './components/Pages/Favorites/Favorites';
 import ProductInfo from './components/productInfo/ProductInfo';
-// import Cart from './components/Cart/Cart';
 
-// redux store
-// import store from './Redux/store/store';
 import {useDispatch} from 'react-redux';
 
 //actions
 import {getAllProducts, getAllFavs} from './Redux/actions/shopping';
-
+import { Toaster } from 'react-hot-toast';
 // styles
 import './App.scss';
-import { Toaster } from 'react-hot-toast';
+
 
 const App = (): JSX.Element => {
   const dispatch = useDispatch();
+  
   // redux store when DOM is updated
   useEffect(()=>{
-    // dispatch 'get products' action
     dispatch(getAllProducts());
     dispatch(getAllFavs());
   }, [dispatch])
