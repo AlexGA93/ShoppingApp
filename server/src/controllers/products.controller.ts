8e41ca0f8bc62a4ab6bcd2bae01ccd9df1ad58b9
AlexGA93 <alexgimat@hotmail.com>
2022-05-09 18:54:20 +0200

    get products routes

--- server/src/controllers/products.controller.ts
+++ server/src/controllers/products.controller.ts
@@ -12,22 +12,25 @@ export const getProducts = async(req: Request, res: Response) => await ProductMo
     .then( products => res.status(200).json(products))
     .catch( err => res.status(500).json({msg:err.message}));
 
-export const getProductById = async(req: Request, res: Response) => {
-    
-    let id = req.params.id;
-    console.log(id);
-    
-
-    await ProductModel
-    .findById(id)
+export const getProductById = async(req: Request, res: Response) => await ProductModel
+    .findById(req.params.id)
     .then( products => res.status(200).json(products))
     .catch( err => res.status(500).json({msg:err.message}));
-    
-};
 
 export const getProductByCategory = async(req: Request, res: Response) => await ProductModel
     .find({category:req.params.category})
     .then( (filteredProducts: apiProductType[]) => res.status(200).json(filteredProducts))
     .catch( err => res.status(500).json({msg:err.message}));
     
-    
+export const getProductByRating = async(req: Request, res: Response) => await ProductModel
+    .find({"rating.rate": parseFloat(req.params.rating)})
+    .then( (filteredProducts: apiProductType[]) => res.status(200).json(filteredProducts))
+    .catch( err => res.status(500).json({msg:err.message}));
+
+
+export const getFavoritesProducts = async(req: Request, res: Response) => await ProductModel
+    .find({favorite: true})
+    .then( (filteredProducts: apiProductType[]) => {
+        (filteredProducts.length !== 0) ? res.status(200).json(filteredProducts) : res.status(200).json({msg:'No favorite items in database'})
+    })
+    .catch( err => res.status(500).json({msg:err.message}));
